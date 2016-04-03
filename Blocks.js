6cd4d6a13a4031278e56aebf9431a1cb836c9513
exnnwc <2buzezsn@gmail.com>
2016-04-02 18:08:12 -0700

    Finishing up for the day. Not much progress.

--- Blocks.js
+++ Blocks.js
@@ -1,34 +1,49 @@
 function Blocks (sizeOfX, sizeOfY){	
 	this.color = new Array();
+	this.move = moveBlock;
 	this.pos = new Array();
 	this.sizeOfX = sizeOfX;
 	this.sizeOfY = sizeOfY;
 	
+	this.check = checkIntegrity;
 	this.last = lastBlock;
 	this.spawn = dropBlock;	
 }
 
-
+function checkIntegrity(){	
+	player=this.last();		
+//	console.log(this.pos[player][0]["y"]%2);
+	if (this.pos[player][0]["y"]%2===0){
+		console.log("ADJ1");
+		this.move(player, "down", 1);			
+	} 
+	if (player>0){
+		for (block=0;block<player;block++){		
+			
+			if (this.pos[block][1]["y"]-this.pos[player][0]["y"]===2){
+				console.log("ADJ2");
+				this.move(player, "down", 1);			
+			}
+		}
+	}
+}
 function dropBlock(){	
 	x= Math.floor(this.sizeOfX/2);
 	y= 0;
 	this.pos.push([{x:x, y:y}, {x:x, y:y-1}, {x:x+1, y:y}, {x:x+1, y:y-1}]);
-	this.color.push(1);
-	console.log("DROP", this.pos.length);
+	this.color.push(randomNumber(1,2));
 }
 
 function lastBlock(){
-	return this.pos.length-1;
+	return (this.pos.length-1);
 }
 
 function moveBlock(block, direction, rateOfChange){
-	console.log(this.pos.length, block, direction, rateOfChange);
 	for (pixel=0;pixel<4;pixel++){
 		if (direction==="left"){
 			this.pos[block][pixel]["x"]-=rateOfChange;
 		} else if (direction==="down"){
-			console.log (this.pos);
-			//this.pos[block][pixel]["y"]+=rateOfChange;
+			this.pos[block][pixel]["y"]+=rateOfChange;
 		} else if (direction==="right"){
 			this.pos[block][pixel]["x"]+=rateOfChange;
 		}
--- Game.js
+++ Game.js
@@ -9,16 +9,14 @@ function Game (sizeOfX, sizeOfY){
 	
 
 	this.checkPosition = checkPosition;
-	this.eraseScreen = eraseScreen;
+	this.erasePlayer = erasePlayer;
 	this.fall = fall;
-	this.hitDown = hitDown;
-	this.hitLeft = hitLeft;
-	this.hitRight = hitRight;
-	this.left = goLeft;
-	this.right = goRight;
 	this.loadBlocks = loadBlocks;
+	this.moveLeft = moveLeft;
+	this.moveRight = moveRight;
 	this.run = run;
 	this.show = show;
+	this.tetrimos = tetrimos;
 	
 	for (x=0;x<this.sizeOfX;x++){
 		this.pos.push([]);
@@ -31,62 +29,109 @@ function Game (sizeOfX, sizeOfY){
 	
 }
 function checkPosition(location, direction, units){	
-	if (direction===0){
+
+	leftOrRight=false;
+	upOrDown=false;
+	if (direction===0){	
 		return this.pos[location["x"]][location["y"]];
-	} else if(direction==="down"){
-		return this.pos[location["x"]][location["y"]+units];
-	}
-}
-
-function eraseScreen(){
-	for (x=0;x<this.sizeOfX;x++){	
-		for (y=0;y<this.sizeOfY;y++){
-			this.pos[x][y]=0;
+	} else if(direction.indexOf("left")>-1){
+		if (location["x"]-units<0){
+			return false;
 		}
+		leftOrRight = location["x"]-units;
+	} else if(direction.indexOf("right")>-1){
+		leftOrRight = location["x"]+units;
+	}
+	
+	if(direction.indexOf("down")>-1){		
+		if (location["y"]+units>=this.sizeOfY){
+			return false;
+		}
+		upOrDown = location["y"]+units;
+	}  else if(direction.indexOf("up")>-1){
+		if (location["y"]-units<0){
+			return false;
+		}
+		upOrDown = location["y"]-units;
+	} 
+	
+	console.log(direction, leftOrRight, upOrDown);
+	if (!leftOrRight){
+		console.log("X");
+		x=location["x"];
+	} else if (!upOrDown){
+		console.log("Y");
+		y=location["y"];
+	}
+	
+	if (leftOrRight && upOrDown){
+		console.log("BOTH");
+		x=leftOrRight;
+		y=upOrDown;
+	} 
+	console.log(x,y);
+	return this.pos[x][y];
+}
+
+function erasePlayer(){
+	player = this.blocks.last();
+	for (pixel=0;pixel<4;pixel++){
+		this.pos[this.blocks.pos[player][pixel]["x"]][this.blocks.pos[player][pixel]["y"]]=0;
 	}
 }
 
-function fall(faster){
-	
+function fall(faster){	
 	this.fallRate = faster
 	  ? 2
 	  : 1;	  
 	var player = this.blocks.last();
 	if (this.checkPosition(this.blocks.pos[player][0], "down", this.fallRate)===0 
 	  && this.checkPosition(this.blocks.pos[player][1], "down", this.fallRate)===0){
-		moveBlock(player, "down", this.fallRate);
-	}
-	/*
-	if (this.pos[block][0]["y"]<this.sizeOfY-this.fallRate && this.pos[block][1]["y"]<this.sizeOfY-this.fallRate 
-	  && !hitABlockGoingDown){
-		for(pixel=0;pixel<4;pixel++){
-			this.pos[block][pixel]["y"]+=this.fallRate;
-		}
-	} else if (this.pos[block][0]["y"]>=this.sizeOfY-this.fallRate || this.pos[block][1]["y"]>=this.sizeOfY-this.fallRate 
-	  || hitABlockGoingDown){
+		this.blocks.move(player, "down", this.fallRate);
+	} else if (this.checkPosition(this.blocks.pos[player][0], "down", this.fallRate)!==0 
+	  || this.checkPosition(this.blocks.pos[player][1], "down", this.fallRate)!==0){
 		return false;
-	}*/
+	}	
 	return true;
 }
 
-
-function run(){
-	/*	
-	if (this.left && !this.right){
-		this.blocks.left();
-	} else 	if (!this.left && this.right){
-		this.blocks.right();
-	} 
-	*/
-	if (!this.fall(this.down)){
-		this.blocks.spawn();
-	}
-	this.eraseScreen();
-	this.loadBlocks();
-
-	$("#gameScreen").html(this.show());
+function loadBlocks(){
+	for (block=0;block<this.blocks.pos.length;block++){
+		for(pixel=0;pixel<this.blocks.pos[block].length;pixel++){
+			this.pos[this.blocks.pos[block][pixel]["x"]][this.blocks.pos[block][pixel]["y"]]=this.blocks.color[block];
+		}
+	}	
 }
 
+function moveLeft(){
+	if (this.checkPosition(this.blocks.pos[player][0], "left", 1)===0){
+		this.blocks.move(player, "left", 2);
+	} 
+}
+
+function moveRight(){
+	if (this.checkPosition(this.blocks.pos[player][2], "right", 1)===0){
+		this.blocks.move(player, "right", 2);
+	} 
+}
+
+function run(){
+	this.erasePlayer();
+	if (this.right){
+		this.moveRight();
+	} else if (this.left){
+		this.moveLeft();
+	}	
+	
+	if (!this.fall(this.down)){
+		//this.tetrimos();
+		this.blocks.check();
+		this.blocks.spawn();
+	}
+
+	this.loadBlocks();
+	$("#gameScreen").html(this.show());
+}
 
 function show(){
 	screen = "";
@@ -98,7 +143,7 @@ function show(){
 				screen = screen + " neutral"
 			} else if (this.pos[x][y]==1){
 				screen = screen + " red"
-			} else if (this.pos[x][y]==1){
+			} else if (this.pos[x][y]==2){
 				screen = screen + " blue"
 			}
 			screen = screen + "'></div>";
@@ -108,103 +153,37 @@ function show(){
 	return screen;
 }
 
+function tetrimos(){
+	num=0;
+	player = this.blocks.last();	
+	blockColor=this.blocks.color[player];
+	blockX=this.blocks.pos[player][0]["x"];
+	blockY=this.blocks.pos[player][0]["y"];	
+	distance=2;	
+	left=0;
+	right=0;
+	
 
-
-
-
-
-
-function goLeft(){	
-	block=this.pos.length-1;
-	var hitABlockGoingLeft = this.hitLeft();
-	if (this.pos[block][0]["x"]>0 && !hitABlockGoingLeft){
 		
-		for(pixel=0;pixel<4;pixel++){
-			this.pos[block][pixel]["x"]--;
-			this.pos[block][pixel]["x"]--;
-		}
-		return true;
-	} else if (this.pos[block]["x"]==0 && hitABlockGoingLeft){
-		return false;
+	while(this.checkPosition(this.blocks.pos[player][0], "down", distance) === blockColor){		
+	
+		distance+=2;
+	}
+	
+	verticalBlocks = distance/2;
+	console.log(verticalBlocks);
+	if (verticalBlocks>=4){
+		console.log("standingI");
+	}
+	distance=2;
+	while(this.checkPosition(this.blocks.pos[player][0], "left", distance) === blockColor){		
+		distance+=2;
+	}
+	horizontalBlocks = distance/2;
+	console.log(horizontalBlocks);
+	if (verticalBlocks>=4){
+		console.log("horizontalI");
 	}
 }
 
 
-function goRight(){		
-	block=this.pos.length-1;
-	var hitABlockGoingRight = this.hitLeft();
-	if (this.pos[block][0]["x"]<this.sizeOfX-2 && !hitABlockGoingRight){
-		for(pixel=0;pixel<4;pixel++){
-			this.pos[block][pixel]["x"]++;
-			this.pos[block][pixel]["x"]++;
-		}
-		return true;
-	} else if (this.pos[block]["x"]<=this.sizeOfX-2 && hitABlockGoingRight){
-		return false;
-	}
-}
-
-
-function hitDown(){
-
-	if (this.pos.length<2){
-		return false;
-	}
-	for (blockHit=0;blockHit<this.pos.length-1;blockHit++){
-		for (otherPixel=0;otherPixel<4;otherPixel+=this.fallRate){
-			for (pixel=0;pixel<4;pixel+=this.fallRate){
-				if (this.pos[blockHit][otherPixel]["x"]===this.pos[this.pos.length-1][pixel]["x"] 
-				  && this.pos[blockHit][otherPixel]["y"]===this.pos[this.pos.length-1][pixel]["y"]+this.fallRate){
-					 console.log(this.pos[blockHit][otherPixel]["y"]-this.pos[this.pos.length-1][pixel]["y"], pixel, otherPixel);
-					return true;
-				}
-			}			
-		}
-	}	
-	return false;
-}
-
-
-function hitLeft(){
-
-	if (this.pos.length<2){
-		return false;
-	}
-	for (blockHit=0;blockHit<this.pos.length-1;blockHit++){
-		for (otherPixel=0;otherPixel<4;otherPixel++){
-			for (pixel=0;pixel<4;pixel++){
-				if (this.pos[blockHit][otherPixel]["x"]-this.fallRate===this.pos[this.pos.length-1][pixel]["x"] 
-				  && this.pos[blockHit][otherPixel]["y"]+this.fallRate===this.pos[this.pos.length-1][pixel]["y"]){
-					return true;
-				}
-			}			
-		}
-	}
-	return false;
-}
-
-
-function hitRight(){
-
-	if (this.pos.length<2){
-		return false;
-	}
-	for (blockHit=0;blockHit<this.pos.length-1;blockHit++){
-		for (otherPixel=0;otherPixel<4;otherPixel++){
-			for (pixel=0;pixel<4;pixel++){
-				if (this.pos[blockHit][otherPixel]["x"]===this.pos[this.pos.length-1][pixel]["x"]+this.fallRate
-				  && this.pos[blockHit][otherPixel]["y"]===this.pos[this.pos.length-1][pixel]["y"]+this.fallRate){
-					return true;
-				}
-			}			
-		}
-	}
-	return false;
-}
-function loadBlocks(){
-	for (block=0;block<this.blocks.pos.length;block++){
-		for(pixel=0;pixel<this.blocks.pos[block].length;pixel++){
-			this.pos[this.blocks.pos[block][pixel]["x"]][this.blocks.pos[block][pixel]["y"]]=this.blocks.color[block];
-		}
-	}	
-}
