9961bb5b7b212a12030f9e7cb8857098c9c8157a
exnnwc <2buzezsn@gmail.com>
2016-04-06 19:04:28 -0700

    Working on code to check for tetrimos

--- Game.js
+++ Game.js
@@ -6,18 +6,20 @@ function Game (sizeOfX, sizeOfY){
 	this.right=false;
 	this.sizeOfX = sizeOfX;
 	this.sizeOfY = sizeOfY;
-	
 
+	this.checkLines = checkLines;
 	this.checkPosition = checkPosition;
+
 	this.erasePlayer = erasePlayer;
 	this.fall = fall;
 	this.loadBlocks = loadBlocks;
+	this.moveDirection = moveDirection;
 	this.moveLeft = moveLeft;
 	this.moveRight = moveRight;
 	this.run = run;
 	this.show = show;
 	this.tetrimos = tetrimos;
-	
+
 	for (x=0;x<this.sizeOfX;x++){
 		this.pos.push([]);
 		for (y=0;y<this.sizeOfY;y++){
@@ -26,13 +28,13 @@ function Game (sizeOfX, sizeOfY){
 	}
 	this.blocks = new Blocks(this.sizeOfX, this.sizeOfY);
 	this.blocks.spawn();
-	
-}
-function checkPosition(location, direction, units){	
 
+}
+function checkPosition(location, direction, units){
+	//console.log(location, direction, units);
 	leftOrRight=false;
 	upOrDown=false;
-	if (direction===0){	
+	if (direction===0){
 		return this.pos[location["x"]][location["y"]];
 	} else if(direction.indexOf("left")>-1){
 		if (location["x"]-units<0){
@@ -42,8 +44,8 @@ function checkPosition(location, direction, units){
 	} else if(direction.indexOf("right")>-1){
 		leftOrRight = location["x"]+units;
 	}
-	
-	if(direction.indexOf("down")>-1){		
+
+	if(direction.indexOf("down")>-1){
 		if (location["y"]+units>=this.sizeOfY){
 			return false;
 		}
@@ -53,23 +55,23 @@ function checkPosition(location, direction, units){
 			return false;
 		}
 		upOrDown = location["y"]-units;
-	} 
-	
-	console.log(direction, leftOrRight, upOrDown);
-	if (!leftOrRight){
-		console.log("X");
-		x=location["x"];
-	} else if (!upOrDown){
-		console.log("Y");
-		y=location["y"];
 	}
-	
-	if (leftOrRight && upOrDown){
+
+	//console.log(direction, leftOrRight, upOrDown);
+	if (!leftOrRight && upOrDown){
+		//console.log("X");
+		x=location["x"];
+		y=upOrDown;
+	} else if (!upOrDown && leftOrRight){
+		//console.log("Y");
+		x=leftOrRight;
+		y=location["y"];
+	} else if (leftOrRight && upOrDown){
 		console.log("BOTH");
 		x=leftOrRight;
 		y=upOrDown;
-	} 
-	console.log(x,y);
+	}
+//	console.log(location["x"], location["y"], direction, x,y);
 	return this.pos[x][y];
 }
 
@@ -80,18 +82,18 @@ function erasePlayer(){
 	}
 }
 
-function fall(faster){	
+function fall(faster){
 	this.fallRate = faster
 	  ? 2
-	  : 1;	  
+	  : 1;
 	var player = this.blocks.last();
-	if (this.checkPosition(this.blocks.pos[player][0], "down", this.fallRate)===0 
+	if (this.checkPosition(this.blocks.pos[player][0], "down", this.fallRate)===0
 	  && this.checkPosition(this.blocks.pos[player][1], "down", this.fallRate)===0){
 		this.blocks.move(player, "down", this.fallRate);
-	} else if (this.checkPosition(this.blocks.pos[player][0], "down", this.fallRate)!==0 
+	} else if (this.checkPosition(this.blocks.pos[player][0], "down", this.fallRate)!==0
 	  || this.checkPosition(this.blocks.pos[player][1], "down", this.fallRate)!==0){
 		return false;
-	}	
+	}
 	return true;
 }
 
@@ -100,19 +102,63 @@ function loadBlocks(){
 		for(pixel=0;pixel<this.blocks.pos[block].length;pixel++){
 			this.pos[this.blocks.pos[block][pixel]["x"]][this.blocks.pos[block][pixel]["y"]]=this.blocks.color[block];
 		}
-	}	
+	}
 }
 
 function moveLeft(){
 	if (this.checkPosition(this.blocks.pos[player][0], "left", 1)===0){
 		this.blocks.move(player, "left", 2);
-	} 
+	}
 }
 
 function moveRight(){
 	if (this.checkPosition(this.blocks.pos[player][2], "right", 1)===0){
 		this.blocks.move(player, "right", 2);
-	} 
+	}
+}
+
+function moveDirection(location, direction, units){
+	leftOrRight=false;
+	upOrDown=false;
+	if (direction===0){
+		return location;
+	} else if(direction.indexOf("left")>-1){
+		if (location["x"]-units<0){
+			return false;
+		}
+		leftOrRight = location["x"]-units;
+	} else if(direction.indexOf("right")>-1){
+		leftOrRight = location["x"]+units;
+	}
+
+	if(direction.indexOf("down")>-1){
+		if (location["y"]+units>=this.sizeOfY){
+			return false;
+		}
+		upOrDown = location["y"]+units;
+	}  else if(direction.indexOf("up")>-1){
+		if (location["y"]-units<0){
+			return false;
+		}
+		upOrDown = location["y"]-units;
+	}
+
+	//console.log(direction, leftOrRight, upOrDown);
+	if (!leftOrRight && upOrDown){
+		//console.log("X");
+		x=location["x"];
+		y=upOrDown;
+	} else if (!upOrDown && leftOrRight){
+		//console.log("Y");
+		x=leftOrRight;
+		y=location["y"];
+	} else if (leftOrRight && upOrDown){
+		console.log("BOTH");
+		x=leftOrRight;
+		y=upOrDown;
+	}
+//	console.log(location["x"], location["y"], direction, x,y);
+	return {x:x, y:y};
 }
 
 function run(){
@@ -121,10 +167,10 @@ function run(){
 		this.moveRight();
 	} else if (this.left){
 		this.moveLeft();
-	}	
-	
+	}
+
 	if (!this.fall(this.down)){
-		//this.tetrimos();
+		this.tetrimos();
 		this.blocks.check();
 		this.blocks.spawn();
 	}
@@ -149,41 +195,62 @@ function show(){
 			screen = screen + "'></div>";
 		}
 		screen = screen + "</div>";
-	}	
+	}
 	return screen;
 }
 
 function tetrimos(){
 	num=0;
-	player = this.blocks.last();	
+	player = this.blocks.last();
 	blockColor=this.blocks.color[player];
 	blockX=this.blocks.pos[player][0]["x"];
-	blockY=this.blocks.pos[player][0]["y"];	
-	distance=2;	
-	left=0;
-	right=0;
-	
+	blockY=this.blocks.pos[player][0]["y"];
+	//this.tilesConnected(this.blocks.pos[player][0], blockColor);
+	var horizontal = this.checkLines("horizontal", this.blocks.pos[player][0], blockColor);
+	console.log(horizontal["tiles"]);
+	if (horizontal["distance"] === 2){
 
-		
-	while(this.checkPosition(this.blocks.pos[player][0], "down", distance) === blockColor){		
-	
-		distance+=2;
-	}
-	
-	verticalBlocks = distance/2;
-	console.log(verticalBlocks);
-	if (verticalBlocks>=4){
-		console.log("standingI");
-	}
-	distance=2;
-	while(this.checkPosition(this.blocks.pos[player][0], "left", distance) === blockColor){		
-		distance+=2;
-	}
-	horizontalBlocks = distance/2;
-	console.log(horizontalBlocks);
-	if (verticalBlocks>=4){
-		console.log("horizontalI");
+		vertical1 = this.checkLines("vertical", this.blocks.pos[player][0], blockColor);
+		vertical2 = this.checkLines("vertical", horizontal["tiles"][0], blockColor);
+		console.log (vertical1, vertical2);
+		if (vertical1["distance"]===2 && vertical2["distance"]==2 && vertical1["end"] && vertical2["end"]){
+			console.log("SQUARE");
+		}
+	} else if (horizontal["distance"] === 3){
+
+	} else if (horizontal["distance"] === 4){
+		console.log("LAID DOWN I");
+	} else if (horizontal["distance"] > 4){
+
 	}
 }
 
+function checkLines(axis, location, color){
+	var directionCaptions = new Array();
+	if (axis === "horizontal"){
+		directionCaptions[0] = "left";
+		directionCaptions[1] = "right";
+	} else if (axis === "vertical"){
+		directionCaptions[0] = "up";
+		directionCaptions[1] = "down";
+	}
+	var directionNumbers = [2, 2];
+	var tiles = new Array();
 
+	for (direction in directionNumbers){
+		while (this.checkPosition(location, directionCaptions[direction], directionNumbers[direction])===color){
+			tiles.push (this.moveDirection(location, directionCaptions[direction], directionNumbers[direction]));
+			directionNumbers[direction]+=2;
+		}
+		directionNumbers[direction] = (directionNumbers[direction] / 2 ) - 1;
+	}
+	var distance = directionNumbers[0] + directionNumbers[1] + 1;
+	var end = false;
+	if (directionNumbers[0]!==0 && directionNumbers[1]===0 ){
+		end = directionCaptions[1];
+	} else 	if (directionNumbers[0]===0 && directionNumbers[1]!==0 ){
+			end = directionCaptions[0];
+	}
+	var center = (directionNumbers[0]===directionNumbers[1] && distance%2===1) ;
+	return {tiles:tiles, distance:distance, end:end, center:center};
+}
